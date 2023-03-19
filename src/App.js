e6d5073ab916798aa5ea6897d454603fd08c20dd
tiendndev <ngoctien300401@gmail.com>
2023-03-20 00:35:35 +0700

    separate component

--- src/App.js
+++ src/App.js
@@ -1,119 +1,17 @@
+import React from "react";
 import "./App.scss";
+
+// Custom components
+import AppBar from "components/AppBar/AppBar";
+import BoardBar from "components/BoardBar/BoardBar";
+import BoardContent from "components/BoardContent/BoardContent";
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar app">App Bar</nav>
-      <nav className="navbar board">Board Bar</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://cdn.cdnparenting.com/articles/2019/03/01163811/Tips-and-Tricks-to-Motivate-Yourself-649683652.jpg"
-                alt="tiendndev-img"
-              />
-              Title: tiendndev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://cdn.cdnparenting.com/articles/2019/03/01163811/Tips-and-Tricks-to-Motivate-Yourself-649683652.jpg"
-                alt="tiendndev-img"
-              />
-              Title: tiendndev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://cdn.cdnparenting.com/articles/2019/03/01163811/Tips-and-Tricks-to-Motivate-Yourself-649683652.jpg"
-                alt="tiendndev-img"
-              />
-              Title: tiendndev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://cdn.cdnparenting.com/articles/2019/03/01163811/Tips-and-Tricks-to-Motivate-Yourself-649683652.jpg"
-                alt="tiendndev-img"
-              />
-              Title: tiendndev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://cdn.cdnparenting.com/articles/2019/03/01163811/Tips-and-Tricks-to-Motivate-Yourself-649683652.jpg"
-                alt="tiendndev-img"
-              />
-              Title: tiendndev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                src="https://cdn.cdnparenting.com/articles/2019/03/01163811/Tips-and-Tricks-to-Motivate-Yourself-649683652.jpg"
-                alt="tiendndev-img"
-              />
-              Title: tiendndev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
