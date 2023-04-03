9a7b332db55bc70e52983bdd76920e7f208b21f8
tiendndev <birongze300401@gmail.com>
2023-04-03 16:10:51 +0700

    fix bug error comment in file jsconfig.js

--- src/utilities/sorts.js
+++ src/utilities/sorts.js
@@ -1,5 +1,4 @@
-// Order an array of objects based on another array order
-
+/* Order an array of objects based on another array order */
 export const mapOrder = (array, order, key) => {
    array.sort((a, b) => order.indexOf(a[key]) - order.indexOf(b[key]));
    return array;
